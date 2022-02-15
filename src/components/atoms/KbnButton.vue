8f347a884bcad08d21939af6ff269a7a6e64a3ef
chichchic <pch6789@naver.com>
2022-02-15 13:58:01 +0900

    feat: add KbnButton

---
+++ src/components/atoms/KbnButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button
+    :class="classes"
+    :disabled="disabled"
+    type="button"
+    @click="handleClick"
+  >
+    <slot></slot>
+  </button>
+</template>
+<script>
+export default {
+  name: "KbnButton",
+  props: {
+    type: {
+      type: String,
+      default: "button",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    classes() {
+      const cls = this.type === "text" ? "-text" : "";
+      return [`kbn-button${cls}`];
+    },
+  },
+  methods: {
+    handleClick(e) {
+      this.$emit("click", e);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.kbn-button {
+  padding: 0.6em 1.3em;
+}
+.kbn-button-text {
+  border: none;
+  padding-right: 0;
+  padding-left: 0;
+}
+</style>
---
+++ tests/unit/specs/components/atoms/KbnButton.spec.js
@@ -0,0 +1,94 @@
+import { mount } from "@vue/test-utils";
+import KbnButton from "@/components/atoms/KbnButton";
+
+describe("KbnButton", () => {
+  describe("property", () => {
+    describe("type", () => {
+      describe("default value", () => {
+        it("kbn-button 클래스를 가지는 button 요소로 구성됨", () => {
+          const button = mount(KbnButton);
+          expect(button.is("button")).toEqual(true);
+          expect(button.classes()).toContain("kbn-button");
+        });
+      });
+      describe("button", () => {
+        it("kbn-button 클래스를 갖는 button 요소로 구성됨", () => {
+          const button = mount(KbnButton, {
+            propsData: {
+              type: "button",
+            },
+          });
+          expect(button.is("button")).toEqual(true);
+          expect(button.classes()).toContain("kbn-button");
+        });
+      });
+      describe("text", () => {
+        it("kbn-button-text 클래스를 갖는 button 요소로 구성됨", () => {
+          const button = mount(KbnButton, {
+            propsData: {
+              type: "text",
+            },
+          });
+          expect(button.is("button")).toEqual(true);
+          expect(button.classes()).toContain("kbn-button-text");
+        });
+      });
+      describe("disabled", () => {
+        describe("default value", () => {
+          it("disabled 속성이 부여되지 않음", () => {
+            const button = mount(KbnButton);
+            expect(button.attributes().disabled).toBe(undefined);
+          });
+        });
+
+        describe("true", () => {
+          it("disabled 속성이 부여됨", () => {
+            const button = mount(KbnButton, {
+              propsData: {
+                disabled: true,
+              },
+            });
+            expect(button.attributes().disabled).toBe("disabled");
+          });
+        });
+        describe("false", () => {
+          it("disabled 속성이 부여되지 않음", () => {
+            const button = mount(KbnButton, {
+              propsData: {
+                disabled: false,
+              },
+            });
+            expect(button.attributes().disabled).toBe(undefined);
+          });
+        });
+      });
+      describe("event", () => {
+        describe("click", () => {
+          it("일어나지 않음", () => {
+            const button = mount(KbnButton);
+            button.trigger("click");
+            expect(button.emitted().click.length).toEqual(1);
+          });
+        });
+      });
+      describe("slot", () => {
+        describe("콘텐츠가 있음", () => {
+          it("콘텐츠가 삽입됨", () => {
+            const button = mount(KbnButton, {
+              slots: {
+                default: "<p>hello</p>",
+              },
+            });
+            expect(button.text()).toEqual("hello");
+          });
+        });
+        describe("콘텐츠가 없음", () => {
+          it("콘텐츠가 삽입되지 않음", () => {
+            const button = mount(KbnButton);
+            expect(button.text()).toEqual("");
+          });
+        });
+      });
+    });
+  });
+});
