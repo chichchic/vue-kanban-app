d166e1315affd3695a784f23f741213cd4219f20
chichchic <pch6789@naver.com>
2022-04-11 15:28:42 +0900

    feat: auth code 추가

--- src/router/guards.js
+++ src/router/guards.js
@@ -2,7 +2,6 @@ import store from "../store";
 
 export const authorizeToken = (to, from, next) => {
   if (to.matched.some((record) => record.meta.requiresAuth)) {
-    console.log(store.state.auth, store.state.auth?.token);
     if (!store.state.auth?.token) {
       next({
         path: "/login",
--- src/store/actions.js
+++ src/store/actions.js
@@ -1,9 +1,9 @@
-import auth from "@/api/auth";
+import api from "@/api";
 
 export default {
   async login({ commit }, authInfo) {
     try {
-      const { token, userId } = await auth.login(authInfo);
+      const { token, userId } = await api.Auth.login(authInfo);
       commit("AUTH_LOGIN", { token, userId });
     } catch (err) {
       return err;
